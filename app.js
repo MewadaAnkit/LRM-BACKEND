dbcc32156b8ffc212a4a15c6f7438df3279dd9ea
Ankit Mewada <99315107+MewadaAnkit@users.noreply.github.com>
2024-10-18 21:51:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const bodyParser = require('body-parser')
 const path = require('path')
 //Middlewares
 app.use(cors({
-    origin :["http://localhost:3000/" ,"*"]
+    origin :["http://localhost:3000/" ,"*"],
     credentials : true
 }))
 
